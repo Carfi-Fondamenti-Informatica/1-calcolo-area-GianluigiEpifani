6f982e89bd1f0565258a200ad9ec3d9f03a8cc2f
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-03-12 17:02:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a=0, b=0, c=0, triangolo=0, quadrato=0, rettangolo=0, trapezio=0;
+    std::cout << "Inserire dei valori reali" <<std::endl;
+    cin >> a >> b >> c;
+    triangolo = a*b/2;
+    quadrato = a*a;
+    rettangolo = a*b;
+    trapezio = (a+b)*c/2;
+    std::cout<< " L'area e' " << triangolo << " " << quadrato << " " << rettangolo << " " << trapezio << std::endl;
+    return 0;
 }
